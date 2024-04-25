1a32e0e0fbbff5ecf1c2c2ba2a1d7277405e94c1
tiqdesign <tiqqq24@gmail.com>
2024-04-25 15:03:02 +0300

    server code examples added

--- server.js
+++ server.js
@@ -1,19 +1,46 @@
 import http from "http";
-const PORT = 8000;
+import fs from "fs/promises";
+import url from "url";
+import path from "path";
 
-const server = http.createServer((req, res) => {
+const PORT = process.env.PORT || 8000;
+
+//Get current path
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log(__filename);
+console.log(__dirname);
+
+const server = http.createServer(async (req, res) => {
   //res.setHeader("Content-Type", "text/plain");
   //res.statusCode = 200;
 
-  res.writeHead(200, {
-    "Content-Type": "application/json",
-  });
+  try {
+    if (req.method === "GET") {
+      let file_path;
+      if (req.url === "/") {
+        file_path = path.join(__dirname, "public", "index.html");
+      } else if (req.url === "/about") {
+        file_path = path.join(__dirname, "public", "about.html");
+      } else {
+        throw new Error("Page not found");
+      }
 
-  res.end(
-    JSON.stringify({
-      message: "Hello World",
-    })
-  );
+      const data = await fs.readFile(file_path);
+      res.setHeader("Content-Type", "text/html");
+      res.write(data);
+      res.end();
+    } else {
+      throw new Error("Invalid request method");
+    }
+  } catch (err) {
+    res.writeHead(500, {
+      "Content-Type": "text/plain",
+    });
+    console.log(err);
+    res.end("Server Error");
+  }
 });
 
 server.listen(PORT, () => {
